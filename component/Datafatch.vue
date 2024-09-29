2c0d4b455d141c8cf042da807a8bd323209e55ef
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-09-30 00:34:46 +0600

    data fetch with components

---
+++ component/Datafatch.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const {data: products} = await useFetch('https://fakestoreapi.com/products',{
+    method: 'GET',
+})
+</script>
+
+<template>
+    <h1 class="text-center bg-amber-500 text-5xl">Data Feching</h1>
+    <div v-for="(product,index) in products">
+        <table>
+            <tr>
+                <td class="text-2xl">{{ product.id}}</td>
+                <td class="text-2xl"> {{ product.title }}</td>
+            </tr>
+        </table>
+       
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+    
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,36 @@
 <script lang="ts" setup>
+import Datafatch from '~/component/Datafatch.vue';
 import Home from '~/component/Home.vue';
 useHead({
     title:'Home page'
 })
+const state = useState(()=>({
+    number:1
+}))
+
+const increment =()=>{
+    state.value.number++
+}
+const decriment =()=>{
+
+    state.value.number--
+}
 
 </script>
 
 <template>
     <div>
 <Home></Home>
+<Datafatch></Datafatch>
+
+    <h2>{{ state.number }}</h2>
+        <button class="bg-yellow-300 text-2xl rounded-lg hover:bg-green-900  " 
+        @click="increment" 
+         >Increment</button>
+      
+        <button class="bg-slate-600  rounded-lg  hover:bg-red-900 text-2xl " @click="decriment" v-if="state.number>1">Decrement</button>
+        <button class="bg-slate-600  rounded-lg " @click="decriment" v-else=return 0>Decrement</button>
+ 
 
     </div>
 </template>
